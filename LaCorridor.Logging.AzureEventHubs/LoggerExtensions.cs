1eb16f00611026b4455703ae34d5c5f6ee590ee0
LaCorridor <laCorridor@outlook.com>
2017-11-13 21:49:43 -0800

    Change 'AddEventHubLogger' to 'AddEventHub'

--- LaCorridor.Logging.AzureEventHubs/LoggerExtensions.cs
+++ LaCorridor.Logging.AzureEventHubs/LoggerExtensions.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace LaCorridor.Logging.AzureEventHubs
 {
     public static class LoggerExtensions
     {
+        [Obsolete("Call AddEventHub() instead.", false)]
         public static ILoggerFactory AddEventHubLogger(this ILoggerFactory factory, string eventHubConnectionString, LogLevel minLogLevel = LogLevel.Warning)
+        {
+            return AddEventHub(factory, eventHubConnectionString, minLogLevel);
+        }
+
+        public static ILoggerFactory AddEventHub(this ILoggerFactory factory, string eventHubConnectionString, LogLevel minLogLevel = LogLevel.Warning)
         {
             factory.AddProvider(new EHLoggerProvider(minLogLevel, eventHubConnectionString));
             return factory;
